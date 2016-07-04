97e6bc07f1f165ab9d16156941b2f1f9d690bcc9
Matteo Ronchi <matteo.ronchi@gmail.com>
2016-07-04 16:31:45 +0200

    chore(css-extractor): Added css-extractor module with tests

---
+++ src/css-modules-extractor.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const path = require('path')
+const read = require('fs-readdir-recursive')
+const postcss = require('postcss')
+const modules = require('postcss-modules')
+
+let styles
+let map
+
+const filter = (file) => file.endsWith('css')
+
+const getJSON = source => (cssFileName, json) => {
+  const relative = path.relative(source, cssFileName)
+  map[relative] = json
+}
+
+const store = (result) => styles.push(result.css)
+
+const process = (source, plugins) => (file) => {
+  const fromPath = path.join(source, file)
+  const css = fs.readFileSync(fromPath)
+
+  return postcss(...plugins, modules({ getJSON: getJSON(source) }))
+    .process(css, { from: fromPath })
+    .then(store)
+}
+
+const result = () => ({ styles: styles.join(''), map })
+
+const extract = (source, ...plugins) => {
+  styles = []
+  map = {}
+
+  const files = read(source).filter(filter)
+  const processes = files.map(process(source, plugins))
+
+  return Promise.all(processes).then(result)
+}
+
+module.exports = { extract }
